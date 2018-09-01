7b68226147c969abb513687ca8e9de4e602877fd
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-01 15:17:21 +0200

    Added problems (no solution) for day9-24

--- src/main/java/com/formichelli/dailycodingproblem/util/Node.kt
+++ src/main/java/com/formichelli/dailycodingproblem/util/Node.kt
@@ -1,3 +1,29 @@
 package com.formichelli.dailycodingproblem.util
 
-data class Node<T>(val value: T, val left: Node<T>? = null, val right: Node<T>? = null)
+open class Node<T>(val value: T, val left: Node<T>? = null, val right: Node<T>? = null) {
+    override fun equals(other: Any?): Boolean {
+        if (other !is Node<*>)
+            return false
+
+        if (value?.equals(other.value) != true)
+            return false
+
+        if (left == null && other.left != null)
+            return false
+
+        if (left?.value?.equals(other.left?.value) != true)
+            return false
+
+        if (right == null && other.right != null)
+            return false
+
+        if (right?.value?.equals(other.right?.value) != true)
+            return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = (value?.hashCode() ?: 0) + (left?.hashCode() ?: 0) + (right?.hashCode() ?: 0)
+
+    override fun toString() = "($value,$left,$right)"
+}
