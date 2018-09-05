f84fc5b4ff819ba97050caa9a41449ca2fa006cc
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-05 21:59:07 +0200

    Added solution for day 13

--- src/main/java/com/formichelli/dailycodingproblem/Day13.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day13.kt
@@ -6,7 +6,25 @@ Given an integer k and a string s, find the length of the longest substring that
 For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
 */
 object Day13 {
-    fun solution(k: Int, s: String): Int {
-        return 0
+    fun solution(k: Int, s: String): String {
+        var longest = ""
+        for (i in 0 until s.length) {
+            val longestAtIndex = longestAtIndex(s, i, k)
+            if (longestAtIndex.length > longest.length)
+                longest = longestAtIndex
+        }
+
+        return longest
+    }
+
+    private fun longestAtIndex(s: String, i: Int, k: Int): String {
+        val charSet = mutableSetOf<Char>()
+        for (j in i until s.length) {
+            charSet.add(s[j])
+            if (charSet.size > k)
+                return s.substring(i, j)
+        }
+
+        return s.substring(i)
     }
 }
