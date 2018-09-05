a3bdb7007245f0552faa2819f3007c301c8cdc29
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-05 21:18:15 +0200

    Added solution for day 11

--- src/main/java/com/formichelli/dailycodingproblem/Day11.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day11.kt
@@ -1,7 +1,5 @@
 package com.formichelli.dailycodingproblem
 
-import java.util.*
-
 /*
 Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
 
@@ -10,7 +8,71 @@ For example, given the query string de and the set of strings [dog, deer, deal],
 Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 */
 object Day11 {
+    class SearchNode(private val value: Char) {
+        companion object {
+            const val ALPHABET_SIZE = 'z' - 'a' + 2
+            const val STRING_END = 0.toChar()
+        }
+
+        // one children for each letter plus string end
+        private val children = Array<SearchNode?>(ALPHABET_SIZE) { null }
+
+        fun add(queryString: String) {
+            add(queryString, 0)
+        }
+
+        private fun add(queryString: String, index: Int) {
+            if (index == queryString.length) {
+                children[ALPHABET_SIZE - 1] = SearchNode(STRING_END)
+                return
+            }
+
+            val charAtIndex = queryString[index]
+            val indexOfChar = charAtIndex - 'a'
+            if (children[indexOfChar] == null) {
+                children[indexOfChar] = SearchNode(charAtIndex)
+            }
+
+            children[indexOfChar]?.add(queryString, index + 1)
+        }
+
+        fun getPrefixSet(queryString: String): Set<String> {
+            return getPrefixSet(queryString, 0)
+        }
+
+        private fun getPrefixSet(queryString: String, index: Int): Set<String> {
+            val charAtIndex = queryString[index]
+            val indexOfChar = charAtIndex - 'a'
+            return if (children[indexOfChar] != null) {
+                if (index < queryString.length - 1) {
+                    children[indexOfChar]!!.getPrefixSet(queryString, index + 1)
+                } else {
+                    children[indexOfChar]!!.getStringSet(queryString)
+                }
+            } else {
+                setOf()
+            }
+        }
+
+        private fun getStringSet(prefix: String, stringSet: MutableSet<String> = mutableSetOf()): Set<String> {
+            children.filterNotNull().forEach {
+                if (it.value == STRING_END) {
+                    stringSet.add(prefix)
+                } else {
+                    it.getStringSet(prefix + it.value, stringSet)
+                }
+            }
+
+            return stringSet
+        }
+    }
+
     fun solution(query: String, querySet: Set<String>): Set<String> {
-        return Collections.emptySet()
+        val searchRoot = SearchNode(0.toChar())
+        for (queryString in querySet) {
+            searchRoot.add(queryString)
+        }
+
+        return searchRoot.getPrefixSet(query)
     }
 }
---
+++ src/main/test/com/formichelli/dailycodingproblem/Day11Test.kt
@@ -0,0 +1,29 @@
+package com.formichelli.dailycodingproblem
+
+import com.formichelli.dailycodingproblem.util.Utils
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import java.util.*
+
+@RunWith(Parameterized::class)
+class Day11Test(private val result: Set<String>, private val query: String, private val querySet: Set<String>) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            return listOf(
+                    arrayOf(setOf("deer", "deal"), "de", setOf("dog", "deer", "deal")),
+                    arrayOf(setOf("deer", "dog", "deal"), "d", setOf("dog", "deer", "deal")),
+                    arrayOf(Collections.emptySet<String>(), "f", setOf("dog", "deer", "deal")),
+                    arrayOf(setOf("de", "deer", "deal", "dealing"), "de", setOf("ade", "dog", "de", "deer", "deal", "dealing"))
+            )
+        }
+    }
+
+    @Test
+    fun solution() {
+        Assert.assertEquals(String.format("Prefix set of %s should be %s", query, Utils.prettyPrint(result)), result, Day11.solution(query, querySet))
+    }
+}
