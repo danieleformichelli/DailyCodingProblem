4a4d98ddf056b276d9fe032ab82771044b639bf9
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-06 19:13:54 +0200

    Added solution for day 15

--- src/main/java/com/formichelli/dailycodingproblem/Day15.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day15.kt
@@ -1,12 +1,23 @@
 package com.formichelli.dailycodingproblem
 
-import java.util.stream.Stream
+import java.util.*
+import java.util.stream.IntStream
 
 /*
 Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
 */
 object Day15 {
-    fun solution(elementss: Stream<Any>, index: Int): Any {
-        return 0
+    fun solution(elements: IntStream): Int {
+        var i = 1
+        var element = 0
+        val random = Random()
+        elements.forEach {
+            if (random.nextInt(i) == i - 1) {
+                element = it
+            }
+            ++i
+        }
+
+        return element
     }
 }
