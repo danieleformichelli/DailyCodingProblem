79014569f99b2ddba21f24923d9e92bdbe01d15f
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-08 19:39:04 +0200

    Added problem for day 32

---
+++ src/main/java/com/formichelli/dailycodingproblem/Day32.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem
+
+/*
+Suppose you are given a table of currency exchange rates, represented as a 2D array. Determine whether there is a possible arbitrage: that is, whether there is some sequence of trades you can make, starting with some amount A of any currency, so that you can end up with some amount greater than A of that currency.
+
+There are no transaction costs and you can trade fractional quantities.
+*/
+object Day32 {
+    fun solution(exchangeRates: Array<IntArray>): Int {
+        return 0
+    }
+}
