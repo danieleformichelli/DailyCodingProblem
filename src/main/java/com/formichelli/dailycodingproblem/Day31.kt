f3a21c383239c9e4a95d71e8fc49c3fe193db7e8
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-08 15:38:51 +0200

    Added problems for day 30 and 31

---
+++ src/main/java/com/formichelli/dailycodingproblem/Day31.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem
+
+/*
+The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to change one string to the other. For example, the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+*/
+object Day31 {
+    fun solution(stringA: String, stringB: String): Int {
+        return 0
+    }
+}
