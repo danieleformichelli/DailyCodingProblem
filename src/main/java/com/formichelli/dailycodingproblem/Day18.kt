5a454f25047b9fcc4cf888ef74f10a5c6b6a618e
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-19 19:03:13 +0200

    Added solution for days 18

--- src/main/java/com/formichelli/dailycodingproblem/Day18.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day18.kt
@@ -1,5 +1,7 @@
 package com.formichelli.dailycodingproblem
 
+import java.util.*
+
 /*
 Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
 
@@ -13,6 +15,14 @@ Do this in O(n) time and O(k) space. You can modify the input array in-place and
 */
 object Day18 {
     fun solution(numbers: IntArray, k: Int): IntArray {
-        return intArrayOf()
+        for (i in 0..numbers.size - k) {
+            for (j in 0 until k) {
+                if (numbers[i + j] > numbers[i]) {
+                    numbers[i] = numbers[i + j]
+                }
+            }
+        }
+
+        return Arrays.copyOf(numbers, numbers.size - k)!!
     }
 }
