36592149ac118cb35507f8381b4df6552445d7f1
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-19 21:29:52 +0200

    Added solution for day 19

--- src/main/java/com/formichelli/dailycodingproblem/Day19.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day19.kt
@@ -7,6 +7,29 @@ Given an N by K matrix where the nth row and kth column represents the cost to b
 */
 object Day19 {
     fun solution(costMatrix: Array<IntArray>): Int {
-        return 0
+        return recursiveSolution(costMatrix, 0, 0, -1, Int.MAX_VALUE)
+    }
+
+    private fun recursiveSolution(costMatrix: Array<IntArray>, currentHouseIndex: Int, cumulativeCost: Int, lastColor: Int, currentMinimumCost: Int): Int {
+        // we have build all the houses
+        if (currentHouseIndex == costMatrix.size)
+            return cumulativeCost
+
+        // avoid to explore combination that we already know are more expensive
+        if (cumulativeCost > currentMinimumCost)
+            return Int.MAX_VALUE
+
+        // explore all the possible costs from this node, and calculate the minimum
+        var minimumCostForTree = Int.MAX_VALUE
+        val houseCostPerColor = costMatrix[currentHouseIndex]
+        for (color in 0 until houseCostPerColor.size) {
+            if (color == lastColor)
+                continue
+
+            minimumCostForTree = Math.min(minimumCostForTree, recursiveSolution(costMatrix, currentHouseIndex + 1, cumulativeCost + houseCostPerColor[color], color, currentMinimumCost))
+        }
+
+        // return the minimum among the minimum cost for this tree and the absolute minimum we have found so far
+        return Math.min(minimumCostForTree, currentMinimumCost)
     }
 }
