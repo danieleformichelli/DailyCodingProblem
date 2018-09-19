b064763f08df1f1fd5fe51f0b200a38b19640b04
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-19 19:03:54 +0200

    Added problem for day 43

---
+++ src/main/java/com/formichelli/dailycodingproblem/Day43.kt
@@ -0,0 +1,25 @@
+package com.formichelli.dailycodingproblem
+
+/*
+Implement a stack that has the following methods:
+
+push(val), which pushes an element onto the stack
+pop(), which pops off and returns the topmost element of the stack. If there are no elements in the stack, then it should throw an error or return null.
+max(), which returns the maximum value in the stack currently. If there are no elements in the stack, then it should throw an error or return null.
+Each method should run in constant time.
+*/
+object Day43 {
+    class MaxStack {
+        fun push(value: Int) {
+
+        }
+
+        fun pop(): Int {
+            return 0
+        }
+
+        fun max(): Int {
+            return 0
+        }
+    }
+}
