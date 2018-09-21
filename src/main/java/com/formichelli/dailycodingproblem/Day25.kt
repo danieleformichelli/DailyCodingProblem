f0bea3e4ec20a4851e6b24c2413cc9c07b6fc8bd
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-21 22:05:07 +0200

    Added solution for day 25

--- src/main/java/com/formichelli/dailycodingproblem/Day25.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day25.kt
@@ -12,7 +12,30 @@ For example, given the regular expression "ra." and the string "ray", your funct
 Given the regular expression ".*at" and the string "chat", your function should return true. The same regular expression on the string "chats" should return false.
 */
 object Day25 {
-    fun solution(regex: String, s: String): Boolean {
-        return false
+    fun solution(regex: String, string: String): Boolean {
+        return solutionHelper(regex, 0, string, 0)
+    }
+
+    private fun solutionHelper(regex: String, regexIndex: Int, string: String, stringIndex: Int): Boolean {
+        return if (stringIndex == string.length) {
+            if (regexIndex == regex.length) {
+                // it is a match if end of string and regex reached together
+                true
+            } else {
+                if (regex[regexIndex] == '*') {
+                    // it is a match if end of string and all the remainin regex is asterisks
+                    solutionHelper(regex, regexIndex + 1, string, stringIndex)
+                } else {
+                    false
+                }
+            }
+        } else if (regexIndex == regex.length) {
+            false
+        } else when (regex[regexIndex]) {
+            '.' -> solutionHelper(regex, regexIndex + 1, string, stringIndex + 1)
+            string[stringIndex] -> solutionHelper(regex, regexIndex + 1, string, stringIndex + 1)
+            '*' -> solutionHelper(regex, regexIndex + 1, string, stringIndex + 1) || solutionHelper(regex, regexIndex, string, stringIndex + 1) || solutionHelper(regex, regexIndex + 1, string, stringIndex)
+            else -> false
+        }
     }
 }
