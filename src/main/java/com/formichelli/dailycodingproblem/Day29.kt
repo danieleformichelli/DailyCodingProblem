daaeb1831d05fec7e6ae84e603bcbc3deb7f3c32
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-22 14:31:38 +0200

    Added solution for day 29

--- src/main/java/com/formichelli/dailycodingproblem/Day29.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day29.kt
@@ -7,10 +7,42 @@ Implement run-length encoding and decoding. You can assume the string to be enco
 */
 object Day29 {
     fun encode(string: String): String {
-        return ""
+        if (string.isEmpty()) {
+            return ""
+        }
+
+        val encodedString = StringBuilder()
+        var currentChar = string[0]
+        var currentCharCount = 0
+        string.forEach {
+            if (it != currentChar) {
+                encodedString.append(currentCharCount).append(currentChar)
+                currentChar = it
+                currentCharCount = 1
+            } else {
+                ++currentCharCount
+            }
+        }
+
+        encodedString.append(currentCharCount).append(currentChar)
+        return encodedString.toString()
     }
 
     fun decode(string: String): String {
-        return ""
+        val decodedString = StringBuilder()
+        var currentNumber = 0
+        string.forEach {
+            val digit = it - '0'
+            if (digit in 1..9) {
+                // digit
+                currentNumber = currentNumber * 10 + digit
+            } else {
+                // non digit
+                decodedString.append(it.toString().repeat(currentNumber))
+                currentNumber = 0
+            }
+        }
+
+        return decodedString.toString()
     }
 }
