4a0446b496a586745008ef9fb945123a2f1ee3f3
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-24 22:39:28 +0200

    Added solution for day 33

--- src/main/java/com/formichelli/dailycodingproblem/Day33.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day33.kt
@@ -1,5 +1,8 @@
 package com.formichelli.dailycodingproblem
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 /*
 Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
 
@@ -16,7 +19,33 @@ For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should pri
 2
 */
 object Day33 {
-    fun solution(exchangeRates: List<Int>): Double {
-        return 0.0
+    fun MutableList<Int>.addSorted(newValue: Int) {
+        this.forEachIndexed { index, value ->
+            if (newValue < value) {
+                this.add(index, newValue)
+                return
+            }
+        }
+
+        this.add(newValue)
+    }
+
+    fun solution(numbersStream: List<Int>): List<Double> {
+        val numbers = LinkedList<Int>()
+        val medians = ArrayList<Double>(numbersStream.size)
+        numbersStream.forEach {
+            numbers.addSorted(it)
+            medians.add(computeMedian(numbers))
+        }
+
+        return medians
+    }
+
+    private fun computeMedian(sortedNumbers: List<Int>) = if (sortedNumbers.size % 2 != 0) {
+        // middle element
+        sortedNumbers[sortedNumbers.size / 2].toDouble()
+    } else {
+        // average of the 2 middle elements
+        (sortedNumbers[sortedNumbers.size / 2] + sortedNumbers[sortedNumbers.size / 2 - 1]) / 2.0
     }
 }
