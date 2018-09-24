50632225115c59a3d27054acaca2abe5f2dbacb3
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-24 22:14:29 +0200

    Added solution for day 32

--- src/main/java/com/formichelli/dailycodingproblem/Day32.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day32.kt
@@ -6,7 +6,28 @@ Suppose you are given a table of currency exchange rates, represented as a 2D ar
 There are no transaction costs and you can trade fractional quantities.
 */
 object Day32 {
-    fun solution(exchangeRates: Array<IntArray>): Int {
-        return 0
+    fun solution(exchangeRates: Array<DoubleArray>): Boolean {
+        val visitedList = BooleanArray(exchangeRates.size)
+        return (0 until exchangeRates.size).any { currency ->
+            solutionHelper(exchangeRates, currency, currency, visitedList, 1.0)
+        }
+    }
+
+    private fun solutionHelper(exchangeRates: Array<DoubleArray>, startCurrency: Int, currentCurrency: Int, visitedList: BooleanArray, totalExchangeRate: Double): Boolean {
+        visitedList[currentCurrency] = true
+
+        for (i in 0 until exchangeRates.size) {
+            val rateForIndex = totalExchangeRate * exchangeRates[currentCurrency][i]
+            if (i == startCurrency) {
+                if (rateForIndex < 1.0) {
+                    return true
+                }
+            } else if (!visitedList[i] && solutionHelper(exchangeRates, startCurrency, i, visitedList, rateForIndex)) {
+                return true
+            }
+        }
+
+        visitedList[currentCurrency] = false
+        return false
     }
 }
