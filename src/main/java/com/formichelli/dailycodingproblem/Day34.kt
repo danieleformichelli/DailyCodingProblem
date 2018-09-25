566d6caf0590c65e240342150e052961bd7a53e7
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-25 22:08:24 +0200

    Added solution for day 34

--- src/main/java/com/formichelli/dailycodingproblem/Day34.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day34.kt
@@ -8,7 +8,25 @@ For example, given the string "race", you should return "ecarace", since we can 
 As another example, given the string "google", you should return "elgoogle".
 */
 object Day34 {
-    fun solution(s: String): String {
-        return ""
+    fun solution(string: String): String {
+        return solutionHelper(string, 0, string.length - 1)
+    }
+
+    private fun solutionHelper(string: String, fromIndex: Int, toIndex: Int): String = when {
+        toIndex < fromIndex -> ""
+        toIndex == fromIndex -> string[toIndex].toString()
+        string[fromIndex] == string[toIndex] -> string[fromIndex] + solutionHelper(string, fromIndex + 1, toIndex - 1) + string[toIndex]
+        else -> {
+            val palindromeFromLeft = string[toIndex] + solutionHelper(string, fromIndex, toIndex - 1) + string[toIndex]
+            val palindromeFromRight = string[fromIndex] + solutionHelper(string, fromIndex + 1, toIndex) + string[fromIndex]
+
+            if (palindromeFromLeft.length < palindromeFromRight.length) {
+                palindromeFromLeft
+            } else if (palindromeFromRight.length < palindromeFromLeft.length) {
+                palindromeFromRight
+            } else {
+                if (palindromeFromLeft < palindromeFromRight) palindromeFromLeft else palindromeFromRight
+            }
+        }
     }
 }
