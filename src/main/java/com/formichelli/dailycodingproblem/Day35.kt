38630e0011ffd487533ac855ddd2efbfa1e13cf2
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-25 23:04:51 +0200

    Added solution for day 35

--- src/main/java/com/formichelli/dailycodingproblem/Day35.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day35.kt
@@ -9,6 +9,34 @@ For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should beco
 */
 object Day35 {
     fun solution(charArray: CharArray): CharArray {
-        return charArrayOf()
+        var rCount = 0
+        var gCount = 0
+        var bCount = 0
+
+        for (i in 0 until charArray.size) {
+            when (charArray[i]) {
+                'R' -> {
+                    // swap with first B, then with first G
+                    swap(charArray, i, rCount + gCount)
+                    swap(charArray, rCount + gCount, rCount)
+
+                    ++rCount
+                }
+                'G' -> {
+                    // swap with first B
+                    swap(charArray, i, rCount + gCount)
+                    ++gCount
+                }
+                'B' -> ++bCount
+            }
+        }
+
+        return charArray
+    }
+
+    private fun swap(charArray: CharArray, indexA: Int, indexB: Int) {
+        val tmp = charArray[indexA]
+        charArray[indexA] = charArray[indexB]
+        charArray[indexB] = tmp
     }
 }
--- src/main/java/com/formichelli/dailycodingproblem/util/Utils.kt
+++ src/main/java/com/formichelli/dailycodingproblem/util/Utils.kt
@@ -2,5 +2,6 @@ package com.formichelli.dailycodingproblem.util
 
 object Utils {
     fun prettyPrint(intArray: IntArray) = intArray.toList().toString()
+    fun prettyPrint(charArray: CharArray) = charArray.toList().toString()
     fun minOf(vararg numbers: Int) = numbers.min() ?: Int.MAX_VALUE
 }
