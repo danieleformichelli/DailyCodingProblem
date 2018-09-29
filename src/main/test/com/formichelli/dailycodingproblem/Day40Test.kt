93fc552642e91eecf82742c4083de73eaeb44f8a
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-09-29 15:43:55 +0200

    Added partial solution for day 40

---
+++ src/main/test/com/formichelli/dailycodingproblem/Day40Test.kt
@@ -0,0 +1,26 @@
+package com.formichelli.dailycodingproblem
+
+import com.formichelli.dailycodingproblem.util.Utils
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class Day40Test(private val result: Int, private val numbers: IntArray) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            return listOf<Array<Any>>(
+                    arrayOf(1, intArrayOf(6, 1, 3, 3, 3, 6, 6)),
+                    arrayOf(19, intArrayOf(13, 19, 13, 13))
+            )
+        }
+    }
+
+    @Test
+    fun solution() {
+        Assert.assertEquals("$result appears only once in ${Utils.prettyPrint(numbers)}", result, Day40.solution(numbers))
+    }
+}
