3c6c856ab6a1fb9460cd521273a5b105a1042583
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-01 22:43:18 +0200

    Added solution for day 44

--- src/main/java/com/formichelli/dailycodingproblem/day41to50/Day44.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day41to50/Day44.kt
@@ -10,7 +10,49 @@ You may assume each element in the array is distinct.
 For example, a sorted list has zero inversions. The array [2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3). The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms an inversion.
 */
 object Day44 {
-    fun solution(number: IntArray): Int {
-        return 0
+    fun solution(numbers: IntArray): Int {
+        return sortAndCount(numbers, 0, numbers.size - 1)
+    }
+
+    private fun sortAndCount(numbers: IntArray, from: Int, to: Int): Int {
+        if (from == to) {
+            return 0
+        }
+
+        val middleElement = from + (to - from) / 2
+        val countLeft = sortAndCount(numbers, from, middleElement)
+        val countRight = sortAndCount(numbers, middleElement + 1, to)
+        val mergeCount = mergeAndCount(numbers, from, to)
+        return countLeft + countRight + mergeCount
+    }
+
+    private fun mergeAndCount(numbers: IntArray, from: Int, to: Int): Int {
+        val merged = IntArray(to - from + 1)
+        var leftIndex = from
+        val middleElement = from + (to - from) / 2
+        var rightIndex = middleElement + 1
+        var inversionsCount = 0
+        for (i in 0 until merged.size) {
+            merged[i] = when {
+                rightIndex > to -> {
+                    numbers[leftIndex++]
+                }
+                leftIndex > middleElement -> {
+                    numbers[rightIndex++]
+                }
+                numbers[leftIndex] < numbers[rightIndex] -> numbers[leftIndex++]
+                numbers[leftIndex] > numbers[rightIndex] -> {
+                    // each element left in the left side is an inversion
+                    inversionsCount += middleElement - leftIndex + 1
+                    numbers[rightIndex++]
+                }
+                else -> {
+                    throw IllegalArgumentException("Elements must be distinct")
+                }
+            }
+        }
+
+        System.arraycopy(merged, 0, numbers, from, merged.size)
+        return inversionsCount
     }
 }
