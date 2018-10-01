320a301bf99954f0add1892b020738f5ca62b3aa
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-01 08:04:37 +0200

    Added solution for day 41

--- src/main/java/com/formichelli/dailycodingproblem/Day41.kt
+++ src/main/java/com/formichelli/dailycodingproblem/Day41.kt
@@ -1,5 +1,7 @@
 package com.formichelli.dailycodingproblem
 
+import java.util.*
+
 /*
 Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. All flights must be used in the itinerary.
 
@@ -10,7 +12,36 @@ Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 
 Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
 */
 object Day41 {
-    fun solution(flights: List<Pair<String,String>>): List<String> {
-        return listOf()
+    fun solution(flights: List<Pair<String, String>>, startAirport: String): List<String>? {
+        val flightsMap = TreeMap<String, MutableSet<String>>()
+        flights.forEach {
+            flightsMap.computeIfAbsent(it.first) { _ -> TreeSet() }.add(it.second)
+        }
+
+        val itinerary = ArrayList<String>()
+        solutionHelper(flightsMap, startAirport, itinerary)
+        return itinerary
+    }
+
+    private fun solutionHelper(flightsMap: TreeMap<String, MutableSet<String>>, currentAirport: String, currentItinerary: MutableList<String>): List<String>? {
+        currentItinerary.add(currentAirport)
+
+        if (flightsMap.all { it.value.isEmpty() }) {
+            // no flights left
+            return currentItinerary
+        }
+
+        val flightsFromCurrent = flightsMap[currentAirport]
+        flightsFromCurrent?.toSet()?.forEach {
+            flightsFromCurrent.remove(it)
+            val solution = solutionHelper(flightsMap, it, currentItinerary)
+            if (solution != null) {
+                return solution
+            }
+            flightsFromCurrent.add(it)
+        }
+
+        currentItinerary.removeAt(currentItinerary.size - 1)
+        return null
     }
 }
