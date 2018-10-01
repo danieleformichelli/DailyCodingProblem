b504824692a2ea9e4c1e8e9b2d45212c51b3afd1
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-01 19:12:50 +0200

    Added solution for day 42

--- src/main/test/com/formichelli/dailycodingproblem/Day42Test.kt
+++ src/main/test/com/formichelli/dailycodingproblem/Day42Test.kt
@@ -6,13 +6,16 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 
 @RunWith(Parameterized::class)
-class Day42Test(private val result: List<Int>, private val numbers: List<Int>, private val targetSum: Int) {
+class Day42Test(private val result: List<Int>?, private val numbers: List<Int>, private val targetSum: Int) {
     companion object {
         @JvmStatic
         @Parameterized.Parameters
-        fun data(): Collection<Array<Any>> {
-            return listOf(
-                    arrayOf(listOf(12, 9, 2, 1), listOf(12, 1, 61, 5, 9, 2), 24))
+        fun data(): Collection<Array<Any?>> {
+            return listOf<Array<Any?>>(
+                    arrayOf(listOf(12, 9, 2, 1), listOf(12, 1, 61, 5, 9, 2), 24),
+                    arrayOf(null, listOf(12, 1, 61, 5, 9, 2), 36),
+                    arrayOf(listOf(12, 12, 9, 2, 1), listOf(12, 1, 61, 5, 9, 2, 12), 36),
+                    arrayOf(listOf(61, 12, 9, 5, 2), listOf(12, 1, 61, 5, 9, 2), 89))
         }
     }
 
