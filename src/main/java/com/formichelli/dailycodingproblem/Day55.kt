b5f23159a9c83acc889ab5392c565fbf1afea2fa
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-01 19:12:36 +0200

    Added problem for day 55

---
+++ src/main/java/com/formichelli/dailycodingproblem/Day55.kt
@@ -0,0 +1,23 @@
+package com.formichelli.dailycodingproblem
+
+/*
+Implement a URL shortener with the following methods:
+
+shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
+restore(short), which expands the shortened string into the original url. If no such shortened string exists, return null.
+Hint: What if we enter the same URL twice?
+*/
+object Day55 {
+    public class UrlShortener<T> {
+        public fun shorten(url: String): String {
+            return ""
+        }
+
+        public fun resture(shortUrl: String): String {
+            return ""
+        }
+    }
+
+    fun solution(table: Array<IntArray>) {
+    }
+}
