2efc847d9a1ea3558407584237106737facafb02
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-02 08:20:01 +0200

    Added solution for day 46

--- src/main/java/com/formichelli/dailycodingproblem/day41to50/Day46.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day41to50/Day46.kt
@@ -7,6 +7,38 @@ For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The long
 */
 object Day46 {
     fun solution(inputString: String): String {
-        return ""
+        var longestPalindromeStart = 0
+        var longestPalindromeLength = 1
+        for (i in 1 until inputString.length) {
+            // find the longest palindrome with center in i
+            for (j in 1 until Math.min(i + 1, inputString.length - i)) {
+                if (inputString[i - j] == inputString[i + j]) {
+                    val currentPalindromeLength = 1 + 2 * (j)
+                    if (currentPalindromeLength > longestPalindromeLength) {
+                        longestPalindromeStart = i - j
+                        longestPalindromeLength = currentPalindromeLength
+                    }
+                } else {
+                    break
+                }
+            }
+
+            // find the longest palindrome with center in i and i - 1
+            if (inputString[i] == inputString[i - 1]) {
+                for (j in 1 until Math.min(i, inputString.length - i)) {
+                    if (inputString[i - 1 - j] == inputString[i + j]) {
+                        val currentPalindromeLength = 2 + 2 * j
+                        if (currentPalindromeLength > longestPalindromeLength) {
+                            longestPalindromeStart = i - 1 - j
+                            longestPalindromeLength = currentPalindromeLength
+                        }
+                    } else {
+                        break
+                    }
+                }
+            }
+        }
+
+        return inputString.substring(longestPalindromeStart, longestPalindromeStart + longestPalindromeLength)
     }
 }
