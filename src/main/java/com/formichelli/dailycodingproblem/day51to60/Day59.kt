2ba7298aacca80ba945cc99ca61b33361bacbae0
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-05 19:00:08 +0200

    Added problem for day 59

---
+++ src/main/java/com/formichelli/dailycodingproblem/day51to60/Day59.kt
@@ -0,0 +1,9 @@
+package com.formichelli.dailycodingproblem.day51to60
+
+/*
+Implement a file syncing algorithm for two computers over a low-bandwidth network. What if we know the files in the two computers are mostly the same?
+*/
+object Day59 {
+    fun solution() {
+    }
+}
