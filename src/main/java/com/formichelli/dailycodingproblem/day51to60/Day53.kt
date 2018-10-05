807391c29a12f8b5f7400b8f8e8391e90285cf24
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-05 19:08:33 +0200

    Added solution for day 53

--- src/main/java/com/formichelli/dailycodingproblem/day51to60/Day53.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day51to60/Day53.kt
@@ -1,16 +1,29 @@
 package com.formichelli.dailycodingproblem.day51to60
 
+import java.util.*
+
 /*
 Implement a queue using two stacks. Recall that a queue is a FIFO (first-in, first-out) data structure with the following methods: enqueue, which inserts an element into the queue, and dequeue, which removes it.
 */
 object Day53 {
-    public class Queue<T> {
-        public fun enqueue(item: T) {
+    class Queue<T> {
+        private val pushStack = Stack<T>()
+        private val popStack = Stack<T>()
 
+        fun enqueue(item: T) {
+            while (!popStack.isEmpty()) {
+                pushStack.push(popStack.pop())
+            }
+
+            pushStack.push(item)
         }
 
-        public fun dequeue(): T? {
-            return null
+        fun dequeue(): T? {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop())
+            }
+
+            return popStack.pop()
         }
     }
 }
