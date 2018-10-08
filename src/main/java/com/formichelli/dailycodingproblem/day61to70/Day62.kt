8257ac87aab20c0822ce116f591f65bb48e27ea2
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-08 19:33:09 +0200

    Added problem for day 62

---
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day62.kt
@@ -0,0 +1,16 @@
+package com.formichelli.dailycodingproblem.day61to70
+
+/*
+There is an N by M matrix of zeroes. Given N and M, write a function to count the number of ways of starting at the top-left corner and getting to the bottom-right corner. You can only move right or down.
+
+For example, given a 2 by 2 matrix, you should return 2, since there are two ways to get to the bottom-right:
+
+Right, then down
+Down, then right
+Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+*/
+object Day62 {
+    fun solution(N: Int, M: Int) : Int {
+        return 0
+    }
+}
