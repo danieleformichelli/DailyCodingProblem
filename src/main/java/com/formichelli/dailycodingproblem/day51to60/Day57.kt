87ca1c5bfc3bfa50e44ad208df6a5b6f302aaeb8
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-08 19:31:42 +0200

    Added solution for day 57

--- src/main/java/com/formichelli/dailycodingproblem/day51to60/Day57.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day51to60/Day57.kt
@@ -8,7 +8,37 @@ You can assume that there are no spaces at the ends of the string and that there
 For example, given the string "the quick brown fox jumps over the lazy dog" and k = 10, you should return: ["the quick", "brown fox", "jumps over", "the lazy", "dog"]. No string in the list has a length of more than 10.
 */
 object Day57 {
-    fun solution(inputString: String, k: Int): String {
-        return ""
+    fun solution(inputString: String, k: Int): List<String>? {
+        val returnList = mutableListOf<String>()
+        var currentWordIndex = 0
+        var currentLineIndex = 0
+        while (true) {
+            var nextSpaceIndex = inputString.indexOf(' ', currentWordIndex)
+            if (nextSpaceIndex == -1) {
+                nextSpaceIndex = inputString.length
+            }
+
+            when {
+                nextSpaceIndex - currentLineIndex > k -> {
+                    if (currentLineIndex != currentWordIndex && currentWordIndex - currentLineIndex - 1 <= k) {
+                        val line = inputString.substring(currentLineIndex, currentWordIndex - 1)
+                        returnList.add(line)
+                        currentLineIndex = currentWordIndex
+                    } else {
+                        // single word too long to fit in one line
+                        return null
+                    }
+                }
+            }
+
+            if (nextSpaceIndex == inputString.length) {
+                returnList.add(inputString.substring(currentLineIndex, inputString.length))
+                break
+            } else {
+                currentWordIndex = nextSpaceIndex + 1
+            }
+        }
+
+        return returnList
     }
 }
