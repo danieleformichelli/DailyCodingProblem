eefe861de328f829724c1ddee277d3ed5d455678
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-08 18:09:05 +0200

    Added solution for day 55

--- src/main/java/com/formichelli/dailycodingproblem/day51to60/Day55.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day51to60/Day55.kt
@@ -1,5 +1,8 @@
 package com.formichelli.dailycodingproblem.day51to60
 
+import java.util.*
+import kotlin.math.roundToInt
+
 /*
 Implement a URL shortener with the following methods:
 
@@ -8,13 +11,44 @@ restore(short), which expands the shortened string into the original url. If no 
 Hint: What if we enter the same URL twice?
 */
 object Day55 {
-    public class UrlShortener<T> {
-        public fun shorten(url: String): String {
-            return ""
+    class UrlShortener(shortenedLength: Int) {
+        private val shortUrlToUrl = HashMap<String, String>()
+        private val alphabet = CharArray(('z' - 'a' + 1) * 2 + 10)
+        private val dictionarySize = Math.pow(alphabet.size.toDouble(), shortenedLength.toDouble()).roundToInt()
+
+        init {
+            var i = 0
+            for (c in 'a'..'z') {
+                alphabet[i++] = c
+            }
+            for (c in 'A'..'Z') {
+                alphabet[i++] = c
+            }
+            for (c in '0'..'9') {
+                alphabet[i++] = c
+            }
         }
 
-        public fun resture(shortUrl: String): String {
-            return ""
+        fun shorten(url: String): String {
+            var shortUrl: String
+            var clashCount = 0
+            do {
+                val shortenedUrlBuilder = StringBuilder()
+                var hash = Math.abs(url.hashCode() + clashCount++) % dictionarySize
+                while (hash != 0) {
+                    shortenedUrlBuilder.append(alphabet[hash % alphabet.size])
+                    hash /= alphabet.size
+                }
+
+                shortUrl = shortenedUrlBuilder.toString()
+            } while (shortUrlToUrl[shortUrl] != null && shortUrlToUrl[shortUrl] != url)
+
+            shortUrlToUrl[shortUrl] = url
+            return shortUrl
+        }
+
+        fun restore(shortUrl: String): String? {
+            return shortUrlToUrl[shortUrl]
         }
     }
 }
