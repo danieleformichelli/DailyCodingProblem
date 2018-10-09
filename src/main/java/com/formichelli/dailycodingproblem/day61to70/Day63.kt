bc78a0ad688d6291115854d73c6da1f3b0635c0c
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-09 19:12:43 +0200

    Added problem for day 63

---
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day63.kt
@@ -0,0 +1,18 @@
+package com.formichelli.dailycodingproblem.day61to70
+
+/*
+Given a 2D matrix of characters and a target word, write a function that returns whether the word can be found in the matrix by going left-to-right, or up-to-down.
+
+For example, given the following matrix:
+
+[['F', 'A', 'C', 'I'],
+ ['O', 'B', 'Q', 'P'],
+ ['A', 'N', 'O', 'B'],
+ ['M', 'A', 'S', 'S']]
+and the target word 'FOAM', you should return true, since it's the leftmost column. Similarly, given the target word 'MASS', you should return true, since it's the last row.
+*/
+object Day63 {
+    fun solution(characters: Array<CharArray>, targetWord: String) : Boolean {
+        return true
+    }
+}
