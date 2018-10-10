c535d77e1d1993da487d0b4501593b885224950b
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-10 18:18:20 +0200

    Added solution for day 63

--- src/main/java/com/formichelli/dailycodingproblem/day61to70/Day63.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day63.kt
@@ -12,7 +12,43 @@ For example, given the following matrix:
 and the target word 'FOAM', you should return true, since it's the leftmost column. Similarly, given the target word 'MASS', you should return true, since it's the last row.
 */
 object Day63 {
-    fun solution(characters: Array<CharArray>, targetWord: String) : Boolean {
+    fun solution(characters: Array<CharArray>, targetWord: String): Boolean {
+        for (row in 0 until characters.size) {
+            for (column in 0 until characters.size) {
+                if (findWordHorizontal(characters, row, column, targetWord) || findWordVertical(characters, row, column, targetWord)) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    private fun findWordHorizontal(characters: Array<CharArray>, row: Int, column: Int, targetWord: String): Boolean {
+        if (column > characters[row].size - targetWord.length) {
+            return false
+        }
+
+        for (i in 0 until targetWord.length) {
+            if (characters[row][column + i] != targetWord[i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun findWordVertical(characters: Array<CharArray>, row: Int, column: Int, targetWord: String): Boolean {
+        if (row > characters.size - targetWord.length) {
+            return false
+        }
+
+        for (i in 0 until targetWord.length) {
+            if (characters[row + i][column] != targetWord[i]) {
+                return false
+            }
+        }
+
         return true
     }
 }
