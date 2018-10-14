83ad740e586e554f6706afe4ddd144addd4dd515
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-14 16:05:22 +0200

    Added problem for day 67

---
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day67.kt
@@ -0,0 +1,20 @@
+package com.formichelli.dailycodingproblem.day61to70
+
+/*
+Implement an LFU (Least Frequently Used) cache. It should be able to be initialized with a cache size n, and contain the following methods:
+
+set(key, value): sets key to value. If there are already n items in the cache and we are adding a new item, then it should also remove the least frequently used item. If there is a tie, then the least recently used key should be removed.
+get(key): gets the value at key. If no such key exists, return null.
+Each operation should run in O(1) time.
+*/
+object Day67 {
+    class LFUCache<T> {
+        fun set(key: String, value: T) {
+
+        }
+
+        fun get(key: String): T? {
+            return null
+        }
+    }
+}
