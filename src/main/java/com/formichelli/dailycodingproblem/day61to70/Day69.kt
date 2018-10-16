b6e288346e31238fdeaf0d2b0e58464f4a51cab4
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-16 21:08:04 +0200

    Added problems for day 69 and 70

---
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day69.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day61to70
+
+/*
+Given a list of integers, return the largest product that can be made by multiplying any three integers.
+
+For example, if the list is [-10, -10, 5, 2], we should return 500, since that's -10 * -10 * 5.
+
+You can assume the list has at least three integers.
+*/
+object Day69 {
+    fun solution(numbers: List<Int>): Int {
+        return 0
+    }
+}
---
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day70.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day61to70
+
+/*
+A number is considered perfect if its digits sum up to exactly 10.
+
+Given a positive integer n, return the n-th perfect number.
+
+For example, given 1, you should return 19. Given 2, you should return 28.
+*/
+object Day70 {
+    fun solution(n: Int): Int {
+        return 0
+    }
+}
