6064f8f51393dbb9d2ae4fdf9308be8f34e1c93d
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-18 11:06:31 +0200

    Added solution for day 70

--- src/main/java/com/formichelli/dailycodingproblem/day61to70/Day70.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day61to70/Day70.kt
@@ -9,6 +9,27 @@ For example, given 1, you should return 19. Given 2, you should return 28.
 */
 object Day70 {
     fun solution(n: Int): Int {
-        return 0
+        // All perfect number are a multiple of 9 plus 10
+        var perfectNumberCandidate = 19
+        var currentN = 1
+        while (currentN < n) {
+            perfectNumberCandidate += 9
+            if (isPerfect(perfectNumberCandidate)) {
+                ++currentN
+            }
+        }
+
+        return perfectNumberCandidate
+    }
+
+    private fun isPerfect(perfectNumberCandidate: Int): Boolean {
+        var digitsSum = 0
+        var remainingDigits = perfectNumberCandidate
+        while (remainingDigits != 0) {
+            digitsSum += remainingDigits % 10
+            remainingDigits /= 10
+        }
+
+        return digitsSum == 10
     }
 }
