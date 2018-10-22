9608047878177ad127ea7db2100501c5c9f1abba
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-22 19:03:33 +0200

    Added problems for days 72-75

--- src/main/java/com/formichelli/dailycodingproblem/day71to80/Day71.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day71to80/Day71.kt
@@ -2,7 +2,6 @@ package com.formichelli.dailycodingproblem.day71to80
 
 import java.util.*
 
-
 /*
 Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive).
 */
---
+++ src/main/java/com/formichelli/dailycodingproblem/day71to80/Day73.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day71to80
+
+import com.formichelli.dailycodingproblem.util.ListNode
+
+/*
+Given the head of a singly linked list, reverse it in-place.
+*/
+object Day73 {
+    fun <T> solution(head: ListNode<T>): ListNode<T> {
+        return head
+    }
+}
