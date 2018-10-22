302f5b160972fb00c6cb3543478888175b2e6985
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-22 22:53:16 +0200

    Added solution for day 73

--- src/main/java/com/formichelli/dailycodingproblem/day71to80/Day73.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day71to80/Day73.kt
@@ -7,6 +7,15 @@ Given the head of a singly linked list, reverse it in-place.
 */
 object Day73 {
     fun <T> solution(head: ListNode<T>): ListNode<T> {
-        return head
+        var currentNode = head
+        var nextNode = head.next
+        head.next = null
+        while (nextNode != null) {
+            val tmp = nextNode.next
+            nextNode.next = currentNode
+            currentNode = nextNode
+            nextNode = tmp
+        }
+        return currentNode
     }
 }
---
+++ src/main/test/com/formichelli/dailycodingproblem/day71to80/Day73Test.kt
@@ -0,0 +1,24 @@
+package com.formichelli.dailycodingproblem.day71to80
+
+import com.formichelli.dailycodingproblem.util.ListNode
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class Day73Test(private val result: List<Int>, private val head: ListNode<Int>) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            return listOf(
+                    arrayOf(listOf(1, 2, 3, 4, 5), ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))))
+        }
+    }
+
+    @Test
+    fun solution() {
+        Assert.assertEquals("Reverted ${head.toList()} should be $result", result, Day73.solution(head).toList())
+    }
+}
