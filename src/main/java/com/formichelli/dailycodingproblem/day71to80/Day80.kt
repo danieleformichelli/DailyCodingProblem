1f518f8883f253b42c07479b626094dd5042909e
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-27 19:18:56 +0200

    Added problems for day 79 and 80

---
+++ src/main/java/com/formichelli/dailycodingproblem/day71to80/Day80.kt
@@ -0,0 +1,18 @@
+package com.formichelli.dailycodingproblem.day71to80
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+
+    a
+   / \
+  b   c
+ /
+d
+*/
+object Day80 {
+    fun <T> solution(root: TreeNode<T>): T {
+        TODO()
+    }
+}
