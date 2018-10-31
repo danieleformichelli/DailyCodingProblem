455fdfe35eb45013f3339e97b495b363e16973a2
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-10-31 08:03:17 +0100

    Added solution for day 75

--- src/main/java/com/formichelli/dailycodingproblem/day71to80/Day75.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day71to80/Day75.kt
@@ -7,6 +7,21 @@ For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 
 */
 object Day75 {
     fun solution(numbers: IntArray): Int {
-        TODO()
+        return solutionHelper(numbers, 0, Int.MIN_VALUE)
+    }
+
+    private fun solutionHelper(numbers: IntArray, index: Int, currentMax: Int): Int {
+        if (index == numbers.size) {
+            return 0
+        }
+
+        val longestWithNumberAtIndex = if (numbers[index] > currentMax) {
+            1 + solutionHelper(numbers, index + 1, numbers[index])
+        } else {
+            Int.MIN_VALUE
+        }
+        val longestWithoutNumberAtIndex = solutionHelper(numbers, index + 1, currentMax)
+
+        return Math.max(longestWithNumberAtIndex, longestWithoutNumberAtIndex)
     }
 }
