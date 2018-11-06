7f2e96bf5a7e2c1cc22af9830b51a899163a1818
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-06 14:29:41 +0100

    Added solution for day 80

--- src/main/java/com/formichelli/dailycodingproblem/day71to80/Day80.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day71to80/Day80.kt
@@ -13,6 +13,23 @@ d
 */
 object Day80 {
     fun <T> solution(root: TreeNode<T>): T {
-        TODO()
+        return solutionHelper(root).second!!
+    }
+
+    private fun <T> solutionHelper(root: TreeNode<T>?): Pair<Int, T?> {
+        if (root == null) {
+            return Pair(0, null)
+        }
+
+        val deepestLeftNode = solutionHelper(root.left)
+        val deepestRightNode = solutionHelper(root.right)
+
+        return if (deepestLeftNode.second == null && deepestRightNode.second == null) {
+            Pair(1, root.value)
+        } else if (deepestLeftNode.first >= deepestRightNode.first) {
+            Pair(deepestLeftNode.first + 1, deepestLeftNode.second)
+        } else {
+            Pair(deepestRightNode.first + 1, deepestRightNode.second)
+        }
     }
 }
---
+++ src/main/test/com/formichelli/dailycodingproblem/day71to80/Day80Test.kt
@@ -0,0 +1,33 @@
+package com.formichelli.dailycodingproblem.day71to80
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class Day80Test(private val result: Char, private val root: TreeNode<Char>) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            val a = TreeNode('a')
+            val b = TreeNode('b')
+            val c = TreeNode('c')
+            val d = TreeNode('d')
+            a.left = b
+            a.right = c
+            b.left = d
+
+            return listOf(
+                    arrayOf('d', TreeNode('d')),
+                    arrayOf('d', a))
+        }
+    }
+
+    @Test
+    fun solution() {
+        Assert.assertEquals("Deepest node of $root should be $result", result, Day80.solution(root))
+    }
+}
