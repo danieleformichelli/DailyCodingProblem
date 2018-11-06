b5ac032c8f6073336033fad95b9379b2bbbc0219
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-06 21:44:20 +0100

    Added problem for day 90

---
+++ src/main/java/com/formichelli/dailycodingproblem/day81to90/Day90.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day81to90
+
+/*
+Given an integer n and a list of integers l, write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
+*/
+object Day90 {
+    fun solution(n: Int, l: List<Int>): Int {
+        TODO()
+    }
+}
