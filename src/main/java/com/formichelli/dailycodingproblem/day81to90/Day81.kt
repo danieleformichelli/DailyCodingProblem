b6bb518039860d74546aa3e7455d419d6a9c172a
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-06 21:12:13 +0100

    Added solution for day 81

--- src/main/java/com/formichelli/dailycodingproblem/day81to90/Day81.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day81to90/Day81.kt
@@ -8,15 +8,23 @@ Given a mapping of digits to letters (as in a phone number), and a digit string,
 For example if {“2”: [“a”, “b”, “c”], 3: [“d”, “e”, “f”], …} then “23” should return [“ad”, “ae”, “af”, “bd”, “be”, “bf”, “cd”, “ce”, “cf"].
 */
 object Day81 {
-    private val rand = Random()
-    private fun rand7() = rand.nextInt(7) + 1
+    fun solution(number: String, mapping: Map<String, List<String>>): List<String> {
+        val possibleWords = LinkedList<StringBuilder>()
+        mapping[number[0].toString()]!!.forEach {
+            possibleWords.add(StringBuilder(it))
+        }
 
-    fun solution(): Int {
-        var rand5: Int
-        do {
-            rand5 = rand7()
-        } while (rand5 !in 1..5)
+        for (digit in number.substring(1)) {
+            val currentWordsCount = possibleWords.size
+            val mappingsForDigit = mapping[digit.toString()]!!
+            for (currentWordIndex in 1..currentWordsCount) {
+                val word = possibleWords.removeAt(0)
+                for (mappingForDigit in mappingsForDigit) {
+                    possibleWords.add(StringBuilder(word).append(mappingForDigit))
+                }
+            }
+        }
 
-        return rand5
+        return possibleWords.map { it.toString() }
     }
 }
---
+++ src/main/test/com/formichelli/dailycodingproblem/day81to90/Day81Test.kt
@@ -0,0 +1,25 @@
+package com.formichelli.dailycodingproblem.day81to90
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class Day81Test(private val result: List<String>, private val mapping: Map<String, List<String>>, private val number: String) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            val mapping = mapOf("2" to listOf("a", "b", "c"), "3" to listOf("d", "e", "f"), "4" to listOf("g", "h", "i"), "5" to listOf("j", "k", "l"), "6" to listOf("m", "n", "o"), "7" to listOf("p", "q", "r", "s"), "8" to listOf("t", "u", "v"), "9" to listOf("w", "x", "y", "z"))
+            return listOf(
+                    arrayOf(listOf("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"), mapping, "23"),
+                    arrayOf(listOf("pw", "px", "py", "pz", "qw", "qx", "qy", "qz", "rw", "rx", "ry", "rz", "sw", "sx", "sy", "sz"), mapping, "79"))
+        }
+    }
+
+    @Test
+    fun solution() {
+        Assert.assertEquals("$number can map to $result", result, Day81.solution(number, mapping.toSortedMap()))
+    }
+}
