482fb5d52f144890ec6001dd04d06542b1c46662
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-07 18:11:56 +0100

    Added solution for day 85

--- src/main/java/com/formichelli/dailycodingproblem/day81to90/Day85.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day81to90/Day85.kt
@@ -5,6 +5,10 @@ Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, usi
 */
 object Day85 {
     fun solution(x: Int, y: Int, b: Int): Int {
-        TODO()
+        // if b is 0, then -b is 0 which means all bits are set to 0
+        // if b is 1, then -b is -1 which means all bits are set to 1
+        // if b is 0, then b-1 is -1 which means all bits are set to 1
+        // if b is 1, then b-1 is 0 which means all bits are set to 0
+        return x.and(-b).or(y.and(b - 1))
     }
 }
