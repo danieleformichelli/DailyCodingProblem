808286f368b44bc0899b0633792896c42a567a71
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-08 08:05:12 +0100

    Added problem and solution for day 91

---
+++ src/main/test/com/formichelli/dailycodingproblem/day91to100/Day91Test.kt
@@ -0,0 +1,24 @@
+package com.formichelli.dailycodingproblem.day91to100
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class Day91Test(private val result: List<Int>, private val n: Int) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            return listOf(
+                    arrayOf((1..10).toList(), 10),
+                    arrayOf((1..20).toList(), 20))
+        }
+    }
+
+    @Test
+    fun solution() {
+        Assert.assertEquals("Range for n=$n should be $result", result, Day91.solution(n))
+    }
+}
