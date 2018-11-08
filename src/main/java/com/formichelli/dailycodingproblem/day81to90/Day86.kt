a47e7b1d200b183428537ccd1060beec535f9d48
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-08 07:45:12 +0100

    Added solution for day 86

--- src/main/java/com/formichelli/dailycodingproblem/day81to90/Day86.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day81to90/Day86.kt
@@ -7,6 +7,26 @@ For example, given the string "()())()", you should return 1. Given the string "
 */
 object Day86 {
     fun solution(parentheses: String): Int {
-        TODO()
+        return solutionHelper(parentheses, 0, 0)
+    }
+
+    private fun solutionHelper(parentheses: String, index: Int, openParenthesisCount: Int): Int {
+        if (index == parentheses.length) {
+            return if (openParenthesisCount == 0) 0 else parentheses.length
+        }
+
+        return when (parentheses[index]) {
+            '(' -> {
+                val resultRemovingParenthesis = 1 + solutionHelper(parentheses, index + 1, openParenthesisCount)
+                val resultKeepingParenthesis = solutionHelper(parentheses, index + 1, openParenthesisCount + 1)
+                Math.min(resultRemovingParenthesis, resultKeepingParenthesis)
+            }
+            ')' -> {
+                val resultRemovingParenthesis = 1 + solutionHelper(parentheses, index + 1, openParenthesisCount)
+                val resultKeepingParenthesis = if (openParenthesisCount > 0) solutionHelper(parentheses, index + 1, openParenthesisCount - 1) else parentheses.length
+                Math.min(resultRemovingParenthesis, resultKeepingParenthesis)
+            }
+            else -> throw IllegalArgumentException("Only ( or ) are allowed")
+        }
     }
 }
