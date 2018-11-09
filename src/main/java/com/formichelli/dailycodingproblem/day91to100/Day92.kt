7e93b6dfefeba11c6647b983265a30395a34f0bd
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-09 07:44:01 +0100

    Added problem for day 92

---
+++ src/main/java/com/formichelli/dailycodingproblem/day91to100/Day92.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day91to100
+
+/*
+We're given a hashmap with a key courseId and value a list of courseIds, which represents that the prerequsite of courseId is courseIds. Return a sorted ordering of courses such that we can finish all courses.
+
+Return null if there is no such ordering.
+
+For example, given {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []}, should return ['CSC100', 'CSC200', 'CSCS300'].
+*/
+object Day92 {
+    fun solution(presrequisitesMap: Map<String, String>): List<String> {
+        TODO()
+    }
+}
