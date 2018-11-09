eddc642fe49d035d4fc2d628ffeebe513bc6f2e4
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-09 07:56:23 +0100

    Added solution for day 90

--- src/main/java/com/formichelli/dailycodingproblem/day81to90/Day90.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day81to90/Day90.kt
@@ -1,10 +1,20 @@
 package com.formichelli.dailycodingproblem.day81to90
 
+import java.util.*
+
 /*
 Given an integer n and a list of integers l, write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
 */
 object Day90 {
     fun solution(n: Int, l: List<Int>): Int {
-        TODO()
+        val rand = Random()
+
+        // could be more efficient
+        var randNum: Int
+        do {
+            randNum = rand.nextInt(n)
+        } while (l.contains(randNum))
+
+        return randNum
     }
 }
---
+++ src/main/test/com/formichelli/dailycodingproblem/day81to90/Day90Test.kt
@@ -0,0 +1,42 @@
+package com.formichelli.dailycodingproblem.day81to90
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class Day90Test(private val n: Int, private val l: List<Int>) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            return listOf(
+                    arrayOf(10, listOf(0)),
+                    arrayOf(10, listOf(0, 2, 4, 6, 8)),
+                    arrayOf(10, listOf(0, 1, 2, 3, 4, 5, 6, 7, 8)))
+        }
+    }
+
+    @Test
+    fun solution() {
+        val numberOfSamples = 10000
+        val frequencyCount = IntArray(n) { 0 }
+        for (i in 0 until numberOfSamples) {
+            val solution = Day90.solution(n, l)
+            ++frequencyCount[solution]
+        }
+
+        // we assume that all numbers of l are distinct and in (0..n-1)
+        val expectedCount = numberOfSamples / (n - l.size)
+        val expectedCountMax = (expectedCount * 1.1).toInt()
+        val expectedCountMin = (expectedCount * 0.9).toInt()
+        frequencyCount.forEachIndexed { index, value ->
+            if (l.contains(index)) {
+                Assert.assertEquals("Frequency of $index should be 0", 0, value)
+            } else {
+                Assert.assertTrue("Frequency of $index should be in [$expectedCountMin,$expectedCountMax] but it is $value", value in expectedCountMin..expectedCountMax)
+            }
+        }
+    }
+}
