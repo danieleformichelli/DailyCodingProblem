3e588e6b66c86f8629c820875bbf300d32f8e174
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-15 20:37:57 +0100

    Added solution for day 96

--- src/main/java/com/formichelli/dailycodingproblem/day91to100/Day96.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day91to100/Day96.kt
@@ -6,7 +6,26 @@ Given a number in the form of a list of digits, return all possible permutations
 For example, given [1,2,3], return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]].
 */
 object Day96 {
-    fun solution(digits: List<Int>): List<List<Int> > {
-        TODO()
+    fun solution(number: List<Int>): Set<List<Int>> {
+        val permutations = HashSet<List<Int>>()
+        solutionHelper(number, BooleanArray(number.size), ArrayList(), permutations)
+        return permutations
+    }
+
+    private fun solutionHelper(number: List<Int>, used: BooleanArray, currentPermutation: MutableList<Int>, permutations: MutableSet<List<Int>>) {
+        if (currentPermutation.size == number.size) {
+            permutations.add(currentPermutation.toList())
+            return
+        }
+
+        number.forEachIndexed { index, digit ->
+            if (!used[index]) {
+                used[index] = true
+                currentPermutation.add(digit)
+                solutionHelper(number, used, currentPermutation, permutations)
+                currentPermutation.removeAt(currentPermutation.lastIndex)
+                used[index] = false
+            }
+        }
     }
 }
