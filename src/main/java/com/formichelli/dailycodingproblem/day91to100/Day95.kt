11b378f71f540260116bf86c7dbec3632e554859
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-15 08:03:29 +0100

    Added solution for day 95

--- src/main/java/com/formichelli/dailycodingproblem/day91to100/Day95.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day91to100/Day95.kt
@@ -1,5 +1,7 @@
 package com.formichelli.dailycodingproblem.day91to100
 
+import java.util.*
+
 /*
 Given a number represented by a list of digits, find the next greater permutation of a number, in terms of lexicographic ordering. If there is not greater permutation possible, return the permutation with the lowest value/ordering.
 
@@ -9,6 +11,23 @@ Can you perform the operation without allocating extra memory (disregarding the 
 */
 object Day95 {
     fun solution(number: List<Int>): List<Int> {
-        TODO()
+        val nextGreaterPermutation = number.toIntArray()
+        for (outerIndex in number.lastIndex downTo 0) {
+            for (innerIndex in outerIndex - 1 downTo 0) {
+                if (number[outerIndex] > number[innerIndex]) {
+                    // first digit of next greater permutation found
+                    nextGreaterPermutation[outerIndex] = number[innerIndex]
+                    nextGreaterPermutation[innerIndex] = number[outerIndex]
+
+                    // order the digits on the right to get the smallest permutation
+                    Arrays.sort(nextGreaterPermutation, innerIndex + 1, number.size)
+                    return nextGreaterPermutation.toList()
+                }
+            }
+        }
+
+        // no greater permutation found
+        nextGreaterPermutation.sort()
+        return nextGreaterPermutation.toList()
     }
 }
