0979ca2fcc92e514453578a0ebf78175667da035
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-23 16:12:56 +0100

    Added problems for day 106

--- src/main/java/com/formichelli/dailycodingproblem/day101to110/Day106.kt
+++ src/main/java/com/formichelli/dailycodingproblem/day101to110/Day106.kt
@@ -1,10 +1,12 @@
 package com.formichelli.dailycodingproblem.day101to110
 
 /*
-Day105
+Given an integer list where each number represents the number of hops you can make, determine whether you can reach to the last index starting at index 0.
+
+For example, [2, 0, 1, 0] returns true while [1, 1, 0, 1] returns false.
 */
 object Day106 {
-    fun solution(f: () -> Any, N: Int): String {
+    fun solution(numbers: List<Int>): Boolean {
         TODO()
     }
 }
