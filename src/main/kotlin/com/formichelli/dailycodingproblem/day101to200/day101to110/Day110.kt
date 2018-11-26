bf60b6fb57a0b85860ca6d1e609e8a10136c4062
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-26 20:57:15 +0100

    Added problem for day 110

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day101to110/Day110.kt
@@ -0,0 +1,21 @@
+package com.formichelli.dailycodingproblem.day101to200.day101to110
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Given a binary tree, return all paths from the root to leaves.
+
+For example, given the tree
+
+   1
+  / \
+ 2   3
+    / \
+   4   5
+it should return [[1, 2], [1, 3, 4], [1, 3, 5]].
+*/
+object Day110 {
+    fun <T> solution(root: TreeNode<T>): List<List<T>> {
+        TODO()
+    }
+}
