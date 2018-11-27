efcea8db5da4e9dd7d40fca5bd8f36807367a152
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-27 07:55:44 +0100

    Added solution for day 102

--- src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day101to110/Day102.kt
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day101to110/Day102.kt
@@ -1,5 +1,7 @@
 package com.formichelli.dailycodingproblem.day101to200.day101to110
 
+import java.util.*
+
 /*
 Given a list of integers and a number K, return which contiguous elements of the list sum to K.
 
@@ -7,6 +9,21 @@ For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2
 */
 object Day102 {
     fun solution(numbers: List<Int>, K: Int): List<Int> {
-        TODO()
+        val sums = LinkedList<Int>()
+        for (i in 0 until numbers.size) {
+            for (j in 0 until sums.size) {
+                sums[j] += numbers[i]
+            }
+            sums.add(numbers[i])
+            while (sums[0] > K) {
+                sums.removeFirst()
+            }
+
+            if (sums[0] == K) {
+                return numbers.subList(i - (sums.size - 1), i + 1)
+            }
+        }
+
+        return listOf()
     }
 }
