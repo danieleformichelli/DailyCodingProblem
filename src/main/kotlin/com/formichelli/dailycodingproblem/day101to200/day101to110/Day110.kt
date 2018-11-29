dbc00514b6bab2e4aa84196e371006de2902d188
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-11-29 21:33:07 +0100

    Added solution for day 110

--- src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day101to110/Day110.kt
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day101to110/Day110.kt
@@ -16,6 +16,23 @@ it should return [[1, 2], [1, 3, 4], [1, 3, 5]].
 */
 object Day110 {
     fun <T> solution(root: TreeNode<T>): List<List<T>> {
-        TODO()
+        val pathsList = mutableListOf<MutableList<T>>(mutableListOf())
+        solutionHelper(root, pathsList)
+        return pathsList
+    }
+
+    private fun <T> solutionHelper(node: TreeNode<T>, pathsList: MutableList<MutableList<T>>) {
+        pathsList.forEach { it.add(node.value) }
+        if (node.left != null && node.right != null) {
+            val copiedList = mutableListOf<MutableList<T>>()
+            pathsList.forEach { copiedList.add(it.toMutableList()) }
+            solutionHelper(node.left!!, pathsList)
+            solutionHelper(node.right!!, copiedList)
+            pathsList.addAll(copiedList)
+        } else if (node.left != null) {
+            solutionHelper(node.left!!, pathsList)
+        } else if (node.right != null) {
+            solutionHelper(node.right!!, pathsList)
+        }
     }
 }
