6ac9659b7bd818760a6d9c9f566ea80d463f302d
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-12-02 21:28:49 +0100

    Added problems for day 114 and 115

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day114.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day101to200.day111to120
+
+/*
+Given a string and a set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters. For example, given "hello/world:here", return "here/world:hello"
+
+Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
+*/
+object Day114 {
+    fun solution(string: String, delimiters: Set<Char>) : String {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day115.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day101to200.day111to120
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+*/
+object Day115 {
+    fun <T> solution(s: TreeNode<T>, t: TreeNode<T>): Boolean {
+        TODO("not implemented")
+    }
+}
