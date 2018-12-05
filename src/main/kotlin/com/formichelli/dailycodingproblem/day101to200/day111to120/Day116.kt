0955e80a306873ed55b8596f573a6e7eb3d4a549
Daniele Formichelli <daniele.formichelli@gmail.com>
2018-12-05 16:26:16 +0100

    Added problems for day 116 and 117

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day116.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day101to200.day111to120
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+
+That is, generate() should return a tree whose size is unbounded but finite.
+*/
+object Day116 {
+    fun solution(): TreeNode<Int> {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day117.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day101to200.day111to120
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Given a binary tree, return the level of the tree with minimum sum.
+*/
+object Day117 {
+    fun solution(root: TreeNode<Int>): Int {
+        TODO("not implemented")
+    }
+}
