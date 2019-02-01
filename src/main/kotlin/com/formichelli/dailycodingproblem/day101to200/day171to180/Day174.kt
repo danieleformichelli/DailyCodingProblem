ad593fadb81193a9b3fe492a7823589c4cc32a91
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-02-01 07:48:04 +0100

    Added problems for days 173-175

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day171to180/Day174.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day101to200.day171to180
+
+/*
+Describe and give an example of each of the following types of polymorphism:
+
+Ad-hoc polymorphism
+Parametric polymorphism
+Subtype polymorphism
+*/
+object Day174 {
+    fun solution(polymorphismType: String): String {
+        TODO("not implemented")
+    }
+}
