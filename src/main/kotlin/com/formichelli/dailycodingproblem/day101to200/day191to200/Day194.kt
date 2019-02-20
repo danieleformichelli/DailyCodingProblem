2660e0172f7eac5b9fdc80ad02357de4f6823f0d
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-02-20 07:50:15 +0100

    Added problems for days 193-194

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day191to200/Day194.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day101to200.day191to200
+
+/*
+Suppose you are given two lists of n points, one list p1, p2, ..., pn on the line y = 0 and the other list q1, q2, ..., qn on the line y = 1.
+Imagine a set of n line segments connecting each point pi to qi.
+Write an algorithm to determine how many pairs of the line segments intersect.
+*/
+object Day194 {
+    fun solution(points1: List<Int>, points2: List<Int>): Int {
+        TODO("not implemented")
+    }
+}
