a3ba0bc9689e5ea4cbe8f9b335649f0fdf8b66f5
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-17 19:26:20 +0100

    Added solution for day 120

--- src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day120.kt
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day120.kt
@@ -6,9 +6,12 @@ Implement the singleton pattern with a twist. First, instead of storing one inst
 object Day120 {
     class Doubleton private constructor() {
         companion object {
+            private val instances = Array(2) { Doubleton() }
+            private var nextIndex = 0
+
             fun getInstance(): Doubleton {
-                // TODO
-                return Doubleton()
+                nextIndex = (nextIndex + 1) % 2
+                return instances[nextIndex]
             }
         }
     }
