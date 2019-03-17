d49fe22146d5464b0fe4431ded0d4e9fc7cb6651
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-17 18:57:14 +0100

    Added solution for day 118

--- src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day118.kt
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day111to120/Day118.kt
@@ -6,7 +6,26 @@ Given a sorted list of integers, square the elements and give the output in sort
 For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
 */
 object Day118 {
-    fun solution(numbers: List<Int>): Int {
-        TODO("not implemented")
+    fun solution(numbers: List<Int>): List<Int> {
+        val squared = ArrayList<Int>()
+
+        var positiveIndex = 0
+        while (positiveIndex < numbers.size && numbers[positiveIndex] < 0) {
+            positiveIndex++
+        }
+        var negativeIndex = positiveIndex - 1
+
+        while (negativeIndex >= 0 || positiveIndex < numbers.size) {
+            val nextSquaredIndex = when {
+                negativeIndex < 0 -> positiveIndex++
+                positiveIndex >= numbers.size -> negativeIndex--
+                numbers[positiveIndex] < -numbers[negativeIndex] -> positiveIndex++
+                else -> negativeIndex--
+            }
+
+            squared.add(numbers[nextSquaredIndex] * numbers[nextSquaredIndex])
+        }
+
+        return squared
     }
 }
--- src/main/test/com/formichelli/dailycodingproblem/day101to200/day111to120/Day118Test.kt
+++ src/main/test/com/formichelli/dailycodingproblem/day101to200/day111to120/Day118Test.kt
@@ -12,7 +12,10 @@ class Day118Test(private val result: List<Int>, private val numbers: List<Int>) 
         @Parameterized.Parameters
         fun data(): Collection<Array<Any>> {
             return listOf<Array<Any>>(
-                    arrayOf(listOf(0, 4, 4, 9, 81), listOf(-9, -2, 0, 2, 3)))
+                    arrayOf(listOf(0, 4, 4, 9, 81), listOf(-9, -2, 0, 2, 3)),
+                    arrayOf(listOf(0, 4, 9), listOf(0, 2, 3)),
+                    arrayOf(listOf(4, 9), listOf(2, 3)),
+                    arrayOf(listOf(4, 81), listOf(-9, -2)))
         }
     }
 
