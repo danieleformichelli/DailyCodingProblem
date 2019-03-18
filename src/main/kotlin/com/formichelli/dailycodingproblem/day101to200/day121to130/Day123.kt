2e83b65acb26911406382e15af20022490392f0a
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-18 22:32:34 +0100

    Added solution for day 123

--- src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day121to130/Day123.kt
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day101to200/day121to130/Day123.kt
@@ -17,6 +17,45 @@ And here are examples of non-numbers:
 */
 object Day123 {
     fun solution(numberStr: String): Boolean {
-        TODO("not implemented")
+        val startIndex = if (numberStr.startsWith("-") || numberStr.startsWith("+")) 1 else 0
+        var hasNumber = false
+        var hasDot = false
+        var hasNumberBeforeDot = false
+        var hasNumberAfterDot = false
+        var hasE = false
+        var hasNumberBeforeE = false
+        var hasNumberAfterE = false
+        for (i in startIndex until numberStr.length) {
+            when (numberStr[i]) {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+                    hasNumber = true
+                    if (!hasDot) {
+                        hasNumberBeforeDot = true
+                    } else {
+                        hasNumberAfterDot = true
+                    }
+                    if (!hasE) {
+                        hasNumberBeforeE = true
+                    } else {
+                        hasNumberAfterE = true
+                    }
+                }
+                '.' -> hasDot = true
+                'e' -> hasE = true
+                else -> return false
+            }
+        }
+
+        if (hasDot && (!hasNumberBeforeDot || !hasNumberAfterDot)) {
+            // if it has a dot, it must have numbers before and after it
+            return false
+        }
+
+        if (hasE && (!hasNumberBeforeE || !hasNumberAfterE)) {
+            // if it has a e, it must have numbers before and after it
+            return false
+        }
+
+        return hasNumber
     }
 }
