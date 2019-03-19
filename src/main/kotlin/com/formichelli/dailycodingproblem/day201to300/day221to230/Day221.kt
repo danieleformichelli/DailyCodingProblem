77aff93cc91682026f16fbb4fda3b991797ad9d9
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-19 19:10:45 +0100

    Added problems for day 221 and 222

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day221to230/Day221.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day221to230
+
+/*
+Let's define a "sevenish" number to be one which is either a power of 7, or the sum of unique powers of 7. The first few sevenish numbers are 1, 7, 8, 49, and so on. Create an algorithm to find the nth sevenish number.
+*/
+object Day221 {
+    fun solution(n: Int): Int {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day221to230/Day222.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day221to230
+
+/*
+Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+
+For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
+*/
+object Day222 {
+    fun solution(path: String): String {
+        TODO("not implemented")
+    }
+}
