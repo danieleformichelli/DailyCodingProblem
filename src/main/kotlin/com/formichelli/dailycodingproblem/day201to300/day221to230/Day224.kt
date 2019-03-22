fc0c1da7dce97734ae43ce9728b6eca81a9f3c10
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-23 00:36:29 +0100

    Added problems for day 224 and 225

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day221to230/Day224.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day201to300.day221to230
+
+/*
+Given a sorted array, find the smallest positive integer that is not the sum of a subset of the array.
+
+For example, for the input [1, 2, 3, 10], you should return 7.
+
+Do this in O(N) time.
+*/
+object Day224 {
+    fun solution(numbers: IntArray): Int {
+        TODO("not implemented")
+    }
+}
