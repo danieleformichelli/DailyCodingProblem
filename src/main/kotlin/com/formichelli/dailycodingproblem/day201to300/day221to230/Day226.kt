f95f945e718e68684fa1026138dd6fdc2fb3e5ed
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-24 23:58:07 +0100

    Added problems for day 226 and 227

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day221to230/Day226.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day221to230
+
+/*
+You come across a dictionary of sorted words in a language you've never seen before. Write a program that returns the correct order of letters in this language.
+
+For example, given ['xww', 'wxyz', 'wxyw', 'ywx', 'ywz'], you should return ['x', 'z', 'w', 'y'].
+*/
+object Day226 {
+    fun solution(words: List<String>): List<Char> {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day221to230/Day227.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day221to230
+
+/*
+Boggle is a game played on a 4 x 4 grid of letters. The goal is to find as many words as possible that can be formed by a sequence of adjacent letters in the grid, using each cell at most once. Given a game board and a dictionary of valid words, implement a Boggle solver.
+*/
+object Day227 {
+    fun solution(grid: Array<CharArray>, dictionary: List<String>): Int {
+        TODO("not implemented")
+    }
+}
