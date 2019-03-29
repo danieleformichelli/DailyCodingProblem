1b433a70660c0da0ebe6ffca10d2c126d70b4a5e
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-03-29 17:54:07 +0100

    Added problems for day 231-232

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day231to240/Day231.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day231to240
+
+/*
+Given a string with repeated characters, rearrange the string so that no two adjacent characters are the same. If this is not possible, return None.
+
+For example, given "aaabbc", you could return "ababac". Given "aaab", return None.
+*/
+object Day231 {
+    fun solution(s: String): String {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day231to240/Day232.kt
@@ -0,0 +1,26 @@
+package com.formichelli.dailycodingproblem.day201to300.day231to240
+
+/*
+Implement a PrefixMapSum class with the following methods:
+
+insert(key: str, value: int): Set a given key's value in the map. If the key already exists, overwrite the value.
+sum(prefix: str): Return the sum of all values of keys that begin with a given prefix.
+For example, you should be able to run the following code:
+
+mapsum.insert("columnar", 3)
+assert mapsum.sum("col") == 3
+
+mapsum.insert("column", 2)
+assert mapsum.sum("col") == 5
+*/
+object Day232 {
+    class PrefixMapSum {
+        fun insert(key: String, value: String) {
+            TODO("not implemented")
+        }
+
+        fun sum(prefix: String): Int {
+            TODO("not implemented")
+        }
+    }
+}
