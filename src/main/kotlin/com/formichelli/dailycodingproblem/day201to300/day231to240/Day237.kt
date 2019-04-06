b214dc18919b83d291b62a2da297ef62bb76c946
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-06 18:03:56 +0200

    Added problems for days 237-240

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day231to240/Day237.kt
@@ -0,0 +1,19 @@
+package com.formichelli.dailycodingproblem.day201to300.day231to240
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+A tree is symmetric if its data and shape remain unchanged when it is reflected about the root node. The following tree is an example:
+
+        4
+      / | \
+    3   5   3
+  /           \
+9              9
+Given a k-ary tree, determine whether it is symmetric.
+*/
+object Day237 {
+    fun solution(root: TreeNode<Any>): Boolean {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day231to240/Day240.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day231to240
+
+/*
+There are N couples sitting in a row of length 2 * N. They are currently ordered randomly, but would like to rearrange themselves so that each couple's partners can sit side by side.
+
+What is the minimum number of swaps necessary for this to happen?
+*/
+object Day240 {
+    fun solution(N: List<Int>): Int {
+        TODO("not implemented")
+    }
+}
