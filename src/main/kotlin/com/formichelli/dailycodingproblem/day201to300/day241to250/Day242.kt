2eea445b25a629059e93282bb152dd3066a9cae3
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-08 18:35:51 +0200

    Added problem for day 242

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day241to250/Day242.kt
@@ -0,0 +1,20 @@
+package com.formichelli.dailycodingproblem.day201to300.day241to250
+
+/*
+You are given an array of length 24, where each element represents the number of new subscribers during the corresponding hour. Implement a data structure that efficiently supports the following:
+
+update(hour: int, value: int): Increment the element at index hour by value.
+query(start: int, end: int): Retrieve the number of subscribers that have signed up between start and end (inclusive).
+You can assume that all values get cleared at the end of the day, and that you will not be asked for start and end values that wrap around midnight.
+*/
+object Day242 {
+    class SubscriptionData {
+        fun update(hour: Int, value: Int) {
+            TODO("not implemented")
+        }
+
+        fun query(start: Int, end: Int) {
+            TODO("not implemented")
+        }
+    }
+}
