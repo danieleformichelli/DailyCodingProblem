8386bda1d786f0d8d27d9f15f4e0dfa6e30ac299
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-11 07:50:08 +0200

    Added problems for day 243 and 244

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day241to250/Day243.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day241to250
+
+/*
+Given an array of numbers N and an integer k, your task is to split N into k partitions such that the maximum sum of any partition is minimized. Return this sum.
+
+For example, given N = [5, 1, 2, 7, 3, 4] and k = 3, you should return 8, since the optimal partition is [5, 1, 2], [7], [3, 4].
+*/
+object Day243 {
+    fun solution(numbers: IntArray, k: Int): Int {
+        TODO("not implemented")
+    }
+}
