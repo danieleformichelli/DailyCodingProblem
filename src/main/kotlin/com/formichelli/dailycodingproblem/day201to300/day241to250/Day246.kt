22cb9918b2fbd842ba4b1ee9dec8ed3313a99f23
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-13 08:03:52 +0200

    Added problem for day 246

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day241to250/Day246.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day241to250
+
+/*
+Given a list of words, determine whether the words can be chained to form a circle. A word X can be placed in front of another word Y in a circle if the last character of X is same as the first character of Y.
+
+For example, the words ['chair', 'height', 'racket', touch', 'tunic'] can form the following circle: chair --> racket --> touch --> height --> tunic --> chair
+*/
+object Day246 {
+    fun solution(words: List<String>): Boolean {
+        TODO("not implemented")
+    }
+}
