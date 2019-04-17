a994b5445fc063e662d17526182757660b37a5e2
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-17 19:15:42 +0200

    Added solution for day 140

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day241to250/Day250.kt
@@ -0,0 +1,21 @@
+package com.formichelli.dailycodingproblem.day201to300.day241to250
+
+/*
+A cryptarithmetic puzzle is a mathematical game where the digits of some numbers are represented by letters. Each letter represents a unique digit.
+
+For example, a puzzle of the form:
+
+  SEND
++ MORE
+--------
+ MONEY
+may have the solution:
+
+{'S': 9, 'E': 5, 'N': 6, 'D': 7, 'M': 1, 'O', 0, 'R': 8, 'Y': 2}
+Given a three-word puzzle like the one above, create an algorithm that finds a solution.
+*/
+object Day250 {
+    fun solution(first: String, second: String, result: String): Map<Char, Int> {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day251to260/Day251.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day251to260
+
+/*
+Given an array of a million integers between zero and a billion, out of order, how can you efficiently sort it? Assume that you cannot store an array of a billion elements in memory.
+*/
+object Day251 {
+    fun solution(numbers: IntArray): IntArray {
+        TODO("not implemented")
+    }
+}
