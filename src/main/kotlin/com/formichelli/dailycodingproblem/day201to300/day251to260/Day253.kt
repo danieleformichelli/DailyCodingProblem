93104f9f2a5e8d068c6c64bdc7eededa4aff7e99
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-19 22:46:54 +0200

    Added problem for day 253

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day251to260/Day253.kt
@@ -0,0 +1,17 @@
+package com.formichelli.dailycodingproblem.day201to300.day251to260
+
+/*
+Given a string and a number of lines k, print the string in zigzag form. In zigzag, characters are printed out diagonally from top left to bottom right until reaching the kth line, then back up to top right, and so on.
+
+For example, given the sentence "thisisazigzag" and k = 4, you should print:
+
+t     a     g
+ h   s z   a
+  i i   i z
+   s     g
+*/
+object Day253 {
+    fun solution(s: String, lines: Int): List<String> {
+        TODO("not implemented")
+    }
+}
