df6a6d9856dfeb666a7ac8dc12b3dca7d60fff0d
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-20 22:33:09 +0200

    Added problem for day 254

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day251to260/Day254.kt
@@ -0,0 +1,29 @@
+package com.formichelli.dailycodingproblem.day201to300.day251to260
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Recall that a full binary tree is one in which each node is either a leaf node, or has two children. Given a binary tree, convert it to a full one by removing nodes with only one child.
+
+For example, given the following tree:
+
+         0
+      /     \
+    1         2
+  /            \
+3                 4
+  \             /   \
+    5          6     7
+You should convert it to:
+
+     0
+  /     \
+5         4
+        /   \
+       6     7
+*/
+object Day254 {
+    fun <T> solution(root: TreeNode<T>): TreeNode<T> {
+        TODO("not implemented")
+    }
+}
