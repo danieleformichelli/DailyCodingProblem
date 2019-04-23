167a32abeb3a3746062bffcb8a251e8ed7827050
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-23 23:05:44 +0200

    Added problems for days 256 and 257

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day251to260/Day256.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day251to260
+
+import com.formichelli.dailycodingproblem.util.ListNode
+
+/*
+Given a linked list, rearrange the node values such that they appear in alternating low -> high -> low -> high ... form. For example, given 1 -> 2 -> 3 -> 4 -> 5, you should return 1 -> 3 -> 2 -> 5 -> 4.
+*/
+object Day256 {
+    fun solution(graph: ListNode<Int>): ListNode<Int> {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day251to260/Day257.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day251to260
+
+/*
+Given an array of integers out of order, determine the bounds of the smallest window that must be sorted in order for the entire array to be sorted. For example, given [3, 7, 5, 6, 9], you should return (1, 3).
+*/
+object Day257 {
+    fun solution(numbers: IntArray): Pair<Int, Int> {
+        TODO("not implemented")
+    }
+}
