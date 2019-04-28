9ca69b25c0276819c297fd9169e572743d1c1966
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-04-28 21:17:26 +0200

    Added problems for days 261 and 262

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day261to270/Day262.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day261to270
+
+/*
+A bridge in a connected (undirected) graph is an edge that, if removed, causes the graph to become disconnected. Find all the bridges in a graph.
+*/
+object Day262 {
+    fun solution(edges: List<Pair<Int, Int>>): List<Pair<Int, Int>> {
+        TODO("not implemented")
+    }
+}
