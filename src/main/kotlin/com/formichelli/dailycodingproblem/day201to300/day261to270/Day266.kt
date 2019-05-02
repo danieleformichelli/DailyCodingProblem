1b6ee7cf67cab71553a49fd7124bd78e2729cfc8
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-02 20:35:42 +0200

    Added problem for day 266

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day261to270/Day266.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day261to270
+
+/*
+A step word is formed by taking a given word, adding a letter, and anagramming the result. For example, starting with the word "APPLE", you can add an "A" and anagram to get "APPEAL".
+
+Given a dictionary of words and an input word, create a function that returns all valid step words.
+*/
+object Day266 {
+    fun solution(dictionary: Set<String>, word: String): Set<String> {
+        TODO("not implemented")
+    }
+}
