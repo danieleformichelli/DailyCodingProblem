d27dc5105a056077feca2f222828f6b4c53b5229
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-02 07:59:14 +0200

    Added problem for day 265

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day261to270/Day265.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day201to300.day261to270
+
+/*
+MegaCorp wants to give bonuses to its employees based on how many lines of codes they have written. They would like to give the smallest positive amount to each worker consistent with the constraint that if a developer has written more lines of code than their neighbor, they should receive more money.
+
+Given an array representing a line of seats of employees at MegaCorp, determine how much each one should get paid.
+
+For example, given [10, 40, 200, 1000, 60, 30], you should return [1, 2, 3, 4, 2, 1].
+*/
+object Day265 {
+    fun solution(linesOfCode: List<Int>): List<Int> {
+        TODO("not implemented")
+    }
+}
