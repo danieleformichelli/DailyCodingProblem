a7584425f2c6eb2e150f89412ac27c13090ec897
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-09 00:19:33 +0200

    Added problems for days 269 to 272

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day271to280/Day271.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day271to280
+
+/*
+Given a sorted list of integers of length N, determine if an element x is in the list without performing any multiplication, division, or bit-shift operations.
+*/
+object Day271 {
+    fun solution(numbers: List<Int>, x: Int): Boolean {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day271to280/Day272.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day271to280
+
+/*
+Write a function, throw_dice(N, faces, total), that determines how many ways it is possible to throw N dice with some number of faces each to get a specific total.
+
+For example, throw_dice(3, 6, 7) should equal 15.
+*/
+object Day272 {
+    fun solution(N: Int, faces: Int, total: Int): Int {
+        TODO("not implemented")
+    }
+}
