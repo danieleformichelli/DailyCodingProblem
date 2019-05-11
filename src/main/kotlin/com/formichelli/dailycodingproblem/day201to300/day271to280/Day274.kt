5b1bdcc5791d440ebd32ec314243ee55ec21bf3a
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-11 10:27:23 +0200

    Added problems for days 273 and 274

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day271to280/Day274.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day201to300.day271to280
+
+/*
+Given a string consisting of parentheses, single digits, and positive and negative signs, convert the string into a mathematical expression to obtain the answer.
+
+Don't use eval or a similar built-in parser.
+
+For example, given '-1 + (2 + 3)', you should return 4.
+*/
+object Day274 {
+    fun solution(expresssion: String): Int {
+        TODO("not implemented")
+    }
+}
