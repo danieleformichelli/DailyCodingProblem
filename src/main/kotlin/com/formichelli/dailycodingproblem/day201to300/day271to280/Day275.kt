dadb13f2aacdafaa61762edb5f2795fc17d38e72
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-13 23:23:10 +0200

    Added problems for days 275 to 277

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day271to280/Day275.kt
@@ -0,0 +1,19 @@
+package com.formichelli.dailycodingproblem.day201to300.day271to280
+
+/*
+The "look and say" sequence is defined as follows: beginning with the term 1, each subsequent term visually describes the digits appearing in the previous term. The first few terms are as follows:
+
+1
+11
+21
+1211
+111221
+As an example, the fourth term is 1211, since the third term consists of one 2 and one 1.
+
+Given an integer N, print the Nth term of this sequence.
+*/
+object Day275 {
+    fun solution(N: Int): Int {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day271to280/Day276.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day201to300.day271to280
+
+/*
+Implement an efficient string matching algorithm.
+
+That is, given a string of length N and a pattern of length k, write a program that searches for the pattern in the string with less than O(N * k) worst-case time complexity.
+
+If the pattern is found, return the start index of its location. If not, return False.
+*/
+object Day276 {
+    fun solution(string: String, pattern: String): Int {
+        TODO("not implemented")
+    }
+}
