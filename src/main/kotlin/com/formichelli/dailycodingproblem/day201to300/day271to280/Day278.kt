09c9724369fce1a8906cea2d686295291cbc1742
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-14 21:45:57 +0200

    Added problem for day 278

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day271to280/Day278.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day271to280
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Given an integer N, construct all possible binary search trees with N nodes.
+*/
+object Day278 {
+    fun solution(N: Int): Set<TreeNode<Int>> {
+        TODO("not implemented")
+    }
+}
