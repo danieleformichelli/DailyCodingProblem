47e4bae464d030d01a2ebb4c54e88cf0e2a9b964
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-05-21 07:49:28 +0200

    Added problems for days 282 to 284

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day281to290/Day282.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day281to290
+
+/*
+Given an array of integers, determine whether it contains a Pythagorean triplet. Recall that a Pythogorean triplet (a, b, c) is defined by the equation a^2 + b^2 = c^2.
+*/
+object Day282 {
+    fun solution(a: Int, b: Int, c: Int): Boolean {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day281to290/Day284.kt
@@ -0,0 +1,19 @@
+package com.formichelli.dailycodingproblem.day201to300.day281to290
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Two nodes in a binary tree can be called cousins if they are on the same level of the tree but have different parents. For example, in the following diagram 4 and 6 are cousins.
+
+    1
+   / \
+  2   3
+ / \   \
+4   5   6
+Given a binary tree and a particular node, find all cousins of that node.
+*/
+object Day284 {
+    fun <T> solution(root: TreeNode<T>, node: TreeNode<T>): Set<TreeNode<T>> {
+        TODO("not implemented")
+    }
+}
