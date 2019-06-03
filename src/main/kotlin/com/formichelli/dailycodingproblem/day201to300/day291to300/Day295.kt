e730be11e8c06d362784c78db576bc78eb72639b
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-06-03 07:54:32 +0200

    Added problems for days 291 to 297

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day291to300/Day295.kt
@@ -0,0 +1,23 @@
+package com.formichelli.dailycodingproblem.day201to300.day291to300
+
+/*
+Pascal's triangle is a triangular array of integers constructed with the following formula:
+
+The first row consists of the number 1.
+For each subsequent row, each element is the sum of the numbers directly above it, on either side.
+For example, here are the first few rows:
+
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+Given an input k, return the kth row of Pascal's triangle.
+
+Bonus: Can you do this using only O(k) space?
+*/
+object Day295 {
+    fun solution(k: Int): List<Int> {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day291to300/Day296.kt
@@ -0,0 +1,12 @@
+package com.formichelli.dailycodingproblem.day201to300.day291to300
+
+import com.formichelli.dailycodingproblem.util.TreeNode
+
+/*
+Given a sorted array, convert it into a height-balanced binary search tree.
+*/
+object Day296 {
+    fun solution(sorted: List<Int>): TreeNode<Int> {
+        TODO("not implemented")
+    }
+}
