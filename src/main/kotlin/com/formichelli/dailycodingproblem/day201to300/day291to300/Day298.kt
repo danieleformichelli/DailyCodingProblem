ddc6d495527bcdd2cba1e43f99c3ed0ae8db6827
Daniele Formichelli <daniele.formichelli@gmail.com>
2019-06-05 19:01:00 +0200

    Added problems for days 298 to 300

---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day291to300/Day298.kt
@@ -0,0 +1,14 @@
+package com.formichelli.dailycodingproblem.day201to300.day291to300
+
+/*
+A girl is walking along an apple orchard with a bag in each hand. She likes to pick apples from each tree as she goes along, but is meticulous about not putting different kinds of apples in the same bag.
+
+Given an input describing the types of apples she will pass on her path, in order, determine the length of the longest portion of her path that consists of just two types of apple trees.
+
+For example, given the input [2, 1, 2, 3, 3, 1, 3, 5], the longest portion will involve types 1 and 3, with a length of four.
+*/
+object Day298 {
+    fun solution(appleTypes: List<Int>): Int {
+        TODO("not implemented")
+    }
+}
---
+++ src/main/kotlin/com/formichelli/dailycodingproblem/day201to300/day291to300/Day300.kt
@@ -0,0 +1,10 @@
+package com.formichelli.dailycodingproblem.day201to300.day291to300
+
+/*
+On election day, a voting machine writes data in the form (voter_id, candidate_id) to a text file. Write a program that reads this file as a stream and returns the top 3 candidates at any given time. If you find a voter voting more than once, report this as fraud.
+*/
+object Day300 {
+    fun solution(votes: List<Pair<String, String>>): List<String> {
+        TODO("not implemented")
+    }
+}
